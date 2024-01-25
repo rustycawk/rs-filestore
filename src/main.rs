c3ad0d5f83ef8b798cdee37f64c8742928f2ea24
Nikita Matveyev <n.matveev@gosu.kz>
2024-01-25 14:26:05 +0600

    feat: add processing to return resized images on-demand

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use actix_multipart::Multipart;
-use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer};
+use actix_web::{get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Result};
 use actix_web_prom::PrometheusMetricsBuilder;
 use futures_util::StreamExt;
+use image::ImageOutputFormat;
 use openssl::symm::{Cipher, Crypter, Mode};
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 use std::collections::HashMap;
 use std::fs::OpenOptions;
-use std::io::{Read, Result, Write};
+use std::io::{Cursor, Read, Write};
 use std::path::Path;
 
 const KEY: &[u8] = b"\xb5M\xb1\x99\x96&\xdd\x9e\xe6:\xec\xbb\xc6\x81\xfd\xa5\xf7\x98\xc2 _\xc2R^]\xfc~M\xdbx\xfe\xb8";
 const IV: &[u8] = b"cQ\x11\xf7&\xed\x83>\xcd&\xf4shz,x";
 const STORAGE_PATH: &str = "storage/";
 const BASE_URL: &str = "http://localhost:8080/";
+const BIND_ADDRESS: &str = "0.0.0.0:8080";
 
 fn encrypt(data: &[u8]) -> Vec<u8> {
     let cipher = Cipher::aes_256_cbc();
@@ -68,15 +70,64 @@ async fn upload(mut payload: Multipart) -> Result<HttpResponse> {
 }
 
 #[get("/{filename}")]
-async fn get(req: HttpRequest) -> Result<HttpResponse> {
+async fn get(req: HttpRequest) -> Result<HttpResponse, Error> {
     let filename = req.match_info().get("filename").unwrap();
     let mut buffer = Vec::new();
     let mut file = OpenOptions::new()
         .read(true)
-        .open(Path::new("storage/").join(filename))?;
+        .open(Path::new(STORAGE_PATH).join(filename))?;
     file.read_to_end(&mut buffer).expect("Could not read file");
     let decrypted_buffer = decrypt(&buffer);
+    let params: HashMap<_, _> = req
+        .query_string()
+        .split('&')
+        .filter_map(|s| {
+            let mut iter = s.split('=');
+            iter.next()
+                .and_then(|key| iter.next().map(|value| (key, value)))
+        })
+        .collect();
+    if let Some(&resize) = params.get("resize") {
+        if let Some((mut width, mut height)) = parse_resize_param(resize) {
+            let image = image::load_from_memory(&decrypted_buffer)
+                .expect("Failed to load image from buffer");
+            if width == 0 || height == 0 {
+                let aspect_ratio = image.width() as f32 / image.height() as f32;
+                if width == 0 {
+                    width = (height as f32 * aspect_ratio) as u32;
+                } else {
+                    height = (width as f32 / aspect_ratio) as u32;
+                }
+            }
+            let resized_image =
+                image.resize_exact(width, height, image::imageops::FilterType::Nearest);
+            let mut resized_buffer = Vec::new(); // Create a new buffer for the resized image
+            resized_image
+                .write_to(
+                    &mut Cursor::new(&mut resized_buffer),
+                    ImageOutputFormat::Jpeg(90),
+                )
+                .expect("Failed to write resized image to buffer");
+            return Ok(HttpResponse::Ok()
+                .content_type("image/jpeg")
+                .body(resized_buffer));
+        }
+    }
     Ok(HttpResponse::Ok().body(decrypted_buffer))
+}
+
+fn parse_resize_param(param: &str) -> Option<(u32, u32)> {
+    let sizes: Vec<&str> = param.split('x').collect();
+    if sizes.len() == 2 {
+        match (sizes[0].parse::<u32>(), sizes[1].parse::<u32>()) {
+            (Ok(width), Ok(height)) => Some((width, height)),
+            (Ok(width), Err(_)) => Some((width, 0)), // Width specified, height is auto
+            (Err(_), Ok(height)) => Some((0, height)), // Height specified, width is auto
+            _ => None,
+        }
+    } else {
+        None
+    }
 }
 
 #[actix_web::main]
@@ -84,7 +135,6 @@ async fn main() -> Result<()> {
     if !Path::new(STORAGE_PATH).exists() {
         std::fs::create_dir(STORAGE_PATH).unwrap();
     }
-
     let mut labels = HashMap::new();
     labels.insert("label1".to_string(), "value1".to_string());
     let prometheus = PrometheusMetricsBuilder::new("api")
@@ -92,7 +142,6 @@ async fn main() -> Result<()> {
         .const_labels(labels)
         .build()
         .unwrap();
-
     HttpServer::new(move || {
         App::new()
             .wrap(prometheus.clone())
@@ -100,7 +149,7 @@ async fn main() -> Result<()> {
             .service(upload)
             .service(get)
     })
-    .bind("0.0.0.0:8080")?
+    .bind(BIND_ADDRESS)?
     .run()
     .await?;
     Ok(())
